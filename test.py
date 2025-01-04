d55b8b42c5f9b41850cbd2c56c123a4074cd9f10
unknown <mac00990011@gmail.com>
2025-01-04 16:41:53 +0200

    updated read gmail messages program

--- test.py
+++ test.py
@@ -1,15 +1,29 @@
 import imaplib
 import email
+import json
 import traceback
+import string
 
-
-MY_EMAIL = "example@gmail.com"
-# For the password to work use app password to create a specific password
-# to be used for the program. Like the password below created for this specific
-# program. Can be deleted after use.
-password = "*************"
+dictionary = {}
+with open('password.json', 'r') as file:
+    data = json.load(file)
+MY_EMAIL = data['username']
+password = data['password']
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 993  # Standard IMAP SSL port
+
+
+def store_email(frm, subject, count):
+    frm = frm.split("<")
+    frm = frm[0]
+    if '0' not in dictionary:
+        dictionary.setdefault(count, {}).setdefault('From', f"{frm}")
+        dictionary.setdefault(count, {}).setdefault('Subject', f"{subject}")
+    else:
+        dictionary.setdefault(count, {}).setdefault('From', f"{frm}")
+        dictionary.setdefault(count, {}).setdefault('Subject', f"{subject}")
+    with open('dictionary', 'w') as dictfile:
+        json.dump(dictionary, dictfile, indent=4)
 
 def read_email():
     try:
@@ -25,8 +39,8 @@ def read_email():
         mail_ids = data[1]
         id_list = mail_ids[0].split()
         latest_email_id = int(id_list[-1])
-
-        for i in range(latest_email_id, latest_email_id-10, -1):
+        count = 0
+        for i in range(latest_email_id, latest_email_id - 10, -1):
             data = mail.fetch(str(i), '(RFC822)')
             for response_part in data:
                 arr = response_part[0]
@@ -36,6 +50,8 @@ def read_email():
                     email_from = msg['from']
                     print('From : ' + email_from + '\n')
                     print('Subject : ' + email_subject + '\n')
+                    store_email(email_from, email_subject, count)
+                    count = count + 1
         # Close the connection
         mail.logout()
 
